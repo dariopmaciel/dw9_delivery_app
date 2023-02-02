b576c950c099fa50263d4639de23b306cef9cbd9
Dario de Paula MAciel <dariodepaulamaciel@hotmail.com>
2023-02-02 09:36:25 -0300

    recebendo msg de erro de produtos

--- lib/app/repositories/products/products_repository_impl.dart
+++ lib/app/repositories/products/products_repository_impl.dart
@@ -1,8 +1,12 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'dart:developer';
+
+import 'package:dark_week/app/core/exceptions/repository_exception.dart';
 import 'package:dark_week/app/core/rest_client/custom_dio.dart';
 import 'package:dark_week/app/models/product_model.dart';
-
-import './products_repository.dart';
+import 'package:dark_week/app/repositories/products/products_repository.dart';
+import 'package:dio/dio.dart';
 
 class ProductsRepositoryImpl implements ProductsRepository {
   final CustomDio dio;
@@ -13,11 +17,18 @@ class ProductsRepositoryImpl implements ProductsRepository {
 
   @override
   Future<List<ProductModel>> findAllProducts() async {
-    final result = await dio.unauth().get("/products");
-    // se não colocar <ProductModel> irá falar que list de dynamic não é lista de Productmodel
-    //pois não faz o cast automaticamente
-    return result.data
-        .map<ProductModel>((p) => ProductModel.fromMap(p))
-        .toList();
+    try {
+      final result = await dio.unauth().get("/products");
+      // se não colocar <ProductModel> irá falar que list de dynamic não é lista de Productmodel
+      //pois não faz o cast automaticamente
+      return result.data
+          .map<ProductModel>(
+            (p) => ProductModel.fromMap(p),
+          )
+          .toList();
+    } on DioError catch (e, s) {
+      log("Erro ao buscar produtos", error: e, stackTrace: s);
+      throw RepositoryException(message: "Erro ao buscar produtos");
+    }
   }
 }
