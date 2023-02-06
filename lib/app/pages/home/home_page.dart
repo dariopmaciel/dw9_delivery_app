d0af6e6e090480e9e9a88aa6de7b9f998ad2b608
Dario de Paula MAciel <dariodepaulamaciel@hotmail.com>
2023-02-06 10:45:02 -0300

    carregando na home page os produtos

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -3,8 +3,10 @@ import 'package:dark_week/app/core/ui/helpers/messages.dart';
 import 'package:dark_week/app/core/ui/widgets/delivery_appbar.dart';
 import 'package:dark_week/app/models/product_model.dart';
 import 'package:dark_week/app/pages/home/home_controler.dart';
+import 'package:dark_week/app/pages/home/home_state.dart';
 import 'package:dark_week/app/pages/home/widget/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -27,26 +29,25 @@ class _HomePageState extends State<HomePage> with Loader, Messages {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: DeliveryAppbar(),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return DeliveryProductTile(
-                  product: ProductModel(
-                    id: 0,
-                    name: "Lanche X",
-                    description: "DESCRIÇÃO",
-                    price: 25,
-                    image:
-                        "https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800",
-                  ),
-                );
-              },
-            ),
-          )
-        ],
+      body: BlocConsumer<HomeController, HomeState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: state.products.length,
+                  itemBuilder: (context, index) {
+                    final product = state.products[index];
+                    return DeliveryProductTile(
+                      product: product,
+                    );
+                  },
+                ),
+              )
+            ],
+          );
+        },
       ),
     );
   }
--- lib/app/pages/home/home_state.dart
+++ lib/app/pages/home/home_state.dart
@@ -2,7 +2,10 @@
 import 'package:equatable/equatable.dart';
 
 import 'package:dark_week/app/models/product_model.dart';
+import 'package:match/match.dart';
+//part 'home_state.g.dart';
 
+@match
 enum HomeStateStatus {
   initial,
   loading,
