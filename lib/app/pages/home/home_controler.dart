af13db8246d890a459abd7f1c246675baa8f2902
Dario de Paula MAciel <dariodepaulamaciel@hotmail.com>
2023-02-06 11:23:06 -0300

    states de tela

--- lib/app/pages/home/home_controler.dart
+++ lib/app/pages/home/home_controler.dart
@@ -1,4 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
 
 import 'package:dark_week/app/pages/home/home_state.dart';
@@ -17,9 +20,15 @@ class HomeController extends Cubit<HomeState> {
     emit(state.copyWith(status: HomeStateStatus.loading));
     try {
       final products = await _productsRepository.findAllProducts();
+      throw Exception();
       emit(state.copyWith(status: HomeStateStatus.loaded, products: products));
-    } on Exception catch (e) {
-      // TODO
+    } catch (e, s) {
+      log("Erro ao buscar produtos", error: e, stackTrace: s);
+      emit(
+        state.copyWith(
+            status: HomeStateStatus.error,
+            errorMessage: "Erro ao buscar produtos"),
+      );
     }
   }
 }
--- lib/app/pages/home/home_state.dart
+++ lib/app/pages/home/home_state.dart
@@ -11,28 +11,37 @@ enum HomeStateStatus {
   initial,
   loading,
   loaded,
+  error,
 }
 
 class HomeState extends Equatable {
   final HomeStateStatus status;
   final List<ProductModel> products;
+  final String? errorMessage;
 
-  const HomeState(this.status, this.products);
+  const HomeState(
+    this.status,
+    this.products,
+    this.errorMessage,
+  );
 
   const HomeState.initial()
       : status = HomeStateStatus.initial,
-        products = const [];
+        products = const [],
+        errorMessage = null;
 
   @override
-  List<Object?> get props => [status, products];
+  List<Object?> get props => [status, products, errorMessage];
 
   HomeState copyWith({
     HomeStateStatus? status,
     List<ProductModel>? products,
+    String? errorMessage,
   }) {
     return HomeState(
       status ?? this.status,
       products ?? this.products,
+      errorMessage ?? this.errorMessage,
     );
   }
 }
