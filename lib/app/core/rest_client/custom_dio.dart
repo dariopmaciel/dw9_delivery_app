254345767b6760ad17705cd47d3fcc63134ddd0d
Dario de Paula MAciel <dariodepaulamaciel@hotmail.com>
2023-02-21 10:48:17 -0300

    atualizando o intercepctor

--- lib/app/core/rest_client/custom_dio.dart
+++ lib/app/core/rest_client/custom_dio.dart
@@ -1,8 +1,11 @@
 import 'package:dark_week/app/core/config/env/env.dart';
+import 'package:dark_week/app/core/rest_client/interceptor/auth_interceptor.dart';
 import 'package:dio/dio.dart';
 import 'package:dio/native_imp.dart';
 
 class CustomDio extends DioForNative {
+  late AuthInterceptor _authInterceptor;
+
   CustomDio()
       : super(
           BaseOptions(
@@ -11,19 +14,20 @@ class CustomDio extends DioForNative {
             receiveTimeout: 60000,
           ),
         ) {
-    interceptors.add(
-      LogInterceptor(
-        requestBody: true,
-        responseBody: true,
-        requestHeader: true,
-      ),
-    );
+    interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+      requestHeader: true,
+    ));
+    _authInterceptor = AuthInterceptor();
   }
   CustomDio auth() {
+    interceptors.add(_authInterceptor);
     return this;
   }
 
   CustomDio unauth() {
+    interceptors.remove(_authInterceptor);
     return this;
   }
 }
---
+++ lib/app/core/rest_client/interceptor/auth_interceptor.dart
@@ -0,0 +1,26 @@
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthInterceptor extends Interceptor {
+  @override
+  void onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    final sp = await SharedPreferences.getInstance();
+    final accessToken = sp.getString("accessToken");
+
+    options.headers["Autorization"] = "Bearer $accessToken";
+    handler.next(options);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) async {
+    if (err.response?.statusCode == 401) {
+      //Redirecinal o user para tela de Home fazer login novamente
+      final sp = await SharedPreferences.getInstance();
+      sp.clear();
+      handler.next(err);
+    } else {
+      handler.next(err);
+    }
+  }
+}
